4577dd171894e3e6ac7f107c068e01cf2917d822
flebedev77 <flebedev99@gmail.com>
2024-09-11 22:08:00 +0930

    More minor updates

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
     if (!req.file) {
         return res.status(400).send("No file uploaded.");
     }
-    res.redirect(req.file.filename);
+    res.redirect("/uploads");
 });
 
 app.post("/gist-upload", (req, res) => {
